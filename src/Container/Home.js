7c029dfa6b3457a4b9f675e92e7800cf038f39b0
rohan pawar <pawarrohan030@gmail.com>
2022-08-26 10:50:15 +0530

    First Commit

---
+++ src/Container/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DataForm from "../Component/DataForm";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <DataForm />
+    </div>
+  );
+}
